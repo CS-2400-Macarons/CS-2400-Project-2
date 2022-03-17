4c4fadbceec4e61a1f5393ad26ba7b668e946eac
Minh Tran <mtran8011@yahoo.com>
2022-03-17 13:36:11 -0700

    Added Implementation for convertToPostFix

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,7 +1,9 @@
+import java.util.Locale;
+
 public class Calc
 {
 
-    //        Algorithm convertToPostfix(infix)
+//            Algorithm convertToPostfix(infix)
 //// Converts an infix expression to an equivalent postfix expression.
 //            operatorStack = a new empty stack
 //            postfix = a new empty string
@@ -21,10 +23,10 @@ public class Calc
 //                while (!operatorStack.isEmpty() and
 //                precedence of nextCharacter <= precedence of
 //                operatorStack.peek())
-//            {
+//                {
 //                Append operatorStack.peek() to postfix
 //                operatorStack.pop()
-//            }
+//                }
 //            operatorStack.push(nextCharacter)
 //            break
 //                case '( ' :
@@ -50,7 +52,88 @@ public class Calc
 
     public String convertToPostfix(String infix)
     {
-        return null;
+        LinkedStack<String> operatorStack = new LinkedStack<>();
+        String postfix = "";
+        int index = 0;
+
+        while(index != (infix.length() - 1))
+        {
+
+            String nextCharacter = infix.substring(index, index + 1).toLowerCase();
+            if(nextCharacter != " ")
+            {
+                switch (nextCharacter)
+                {
+                    case "a": case "b": case "c": case "d": case "e":
+                    case "f": case "g": case "h": case "i": case "j":
+                    case "k": case "l": case "m": case "n": case "o":
+                    case "p": case "q": case "r": case "s": case "t":
+                    case "u": case "v": case "w": case "x": case "y":
+                    case "z":
+                        postfix += nextCharacter;
+                        break;
+
+                    case "^":
+                        operatorStack.push(nextCharacter);
+                        break;
+
+                    case "+" : case "-" : case "*" : case "/":
+                        int nextCharPrecedence = 0;
+                        int peekPrecedence = 0;
+
+                        if(nextCharacter == "*" || nextCharacter == "/")
+                        {
+                            nextCharPrecedence = 2;
+                        }
+                        else
+                        {
+                            nextCharPrecedence = 1;
+                        }
+
+                        if(operatorStack.peek() == "*" || operatorStack.peek() == "/")
+                        {
+                            peekPrecedence = 2;
+                        }
+                        else
+                        {
+                            peekPrecedence = 1;
+                        }
+
+                        while(!operatorStack.isEmpty() && nextCharPrecedence <= peekPrecedence)
+                        {
+                            postfix += operatorStack.peek();
+                            operatorStack.pop();
+                        }
+                        operatorStack.push(nextCharacter);
+                        break;
+
+                    case "(":
+                        operatorStack.push(nextCharacter);
+                        break;
+
+                    case ")":
+                        String topOperator = operatorStack.pop();
+                        while (topOperator != "(")
+                        {
+                            postfix += topOperator;
+                            topOperator = operatorStack.pop();
+                        }
+                        break;
+
+                    default: break;
+                }
+
+                while (!operatorStack.isEmpty())
+                {
+                    String topOperator = operatorStack.pop();
+                    postfix += topOperator;
+                }
+            }
+
+            index++;
+        }
+
+        return postfix;
     }
 
 //        // Evaluates a postfix expression.
